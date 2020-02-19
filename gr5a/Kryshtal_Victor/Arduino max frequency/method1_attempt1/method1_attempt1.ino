f8c3a2130b81407081d22a4c0889a433de6f2038
Victor Kryshtal <victor.kryshtal@gmail.com>
2020-02-19 23:57:08 +0200

    Arduino max frequency

---
+++ gr5a/Kryshtal_Victor/Arduino max frequency/method1_attempt1/method1_attempt1.ino
@@ -0,0 +1,8 @@
+void setup() {
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(12, HIGH);
+  digitalWrite(12, LOW);
+}
---
+++ gr5a/Kryshtal_Victor/Arduino max frequency/method2_modified/method2_modified.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  OCR1A = 2;
+  TCCR1A = (1 << COM1A0);
+  TCCR1B = (1 << WGM12) | (1 << CS10);
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  digitalWrite(13, LOW);
+}
