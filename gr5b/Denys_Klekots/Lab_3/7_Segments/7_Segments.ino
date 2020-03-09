4fc585667558e83fdd2f445d37c6131a4c110f65
Denys_Klekots <klekots.denis@gmail.com>
2020-03-09 09:28:07 +0200

    Denys_Klekots Lab_1 & Lab_3 Repoets

---
+++ gr5b/Denys_Klekots/Lab_3/7_Segments/7_Segments.ino
@@ -0,0 +1,111 @@
+void setup() {
+
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+
+  
+}
+
+void number (int n){
+
+  digitalWrite (10,HIGH);
+
+  for(int i=2;i<10;i++){
+    digitalWrite (i,LOW);
+  }
+  
+  switch (n){
+    
+    case 1:
+      digitalWrite (5,HIGH);
+      digitalWrite (9,HIGH);
+      break;
+    case 2:
+      digitalWrite (4,HIGH);
+      digitalWrite (5,HIGH);
+      digitalWrite (6,HIGH);
+      digitalWrite (7,HIGH);
+      digitalWrite (8,HIGH);
+      break;
+    case 3:
+      digitalWrite (4,HIGH);
+      digitalWrite (5,HIGH);
+      digitalWrite (6,HIGH);
+      digitalWrite (8,HIGH);
+      digitalWrite (9,HIGH);
+      break;
+    case 4:
+      digitalWrite (3,HIGH);
+      digitalWrite (5,HIGH);
+      digitalWrite (6,HIGH);
+      digitalWrite (9,HIGH);
+      break;
+    case 5:
+      digitalWrite (3,HIGH);
+      digitalWrite (4,HIGH);
+      digitalWrite (6,HIGH);
+      digitalWrite (8,HIGH);
+      digitalWrite (9,HIGH);
+      break;
+    case 6:
+      digitalWrite (3,HIGH);
+      digitalWrite (4,HIGH);
+      digitalWrite (6,HIGH);
+      digitalWrite (7,HIGH);
+      digitalWrite (8,HIGH);
+      digitalWrite (9,HIGH);
+      break;
+    case 7:
+      digitalWrite (4,HIGH);
+      digitalWrite (5,HIGH);
+      digitalWrite (9,HIGH);
+      break;
+    case 8:
+      digitalWrite (3,HIGH);
+      digitalWrite (4,HIGH);
+      digitalWrite (5,HIGH);
+      digitalWrite (6,HIGH);
+      digitalWrite (7,HIGH);
+      digitalWrite (8,HIGH);
+      digitalWrite (9,HIGH);
+      break;
+    case 9:
+      digitalWrite (3,HIGH);
+      digitalWrite (4,HIGH);
+      digitalWrite (5,HIGH);
+      digitalWrite (6,HIGH);
+      digitalWrite (8,HIGH);
+      digitalWrite (9,HIGH);
+      break;
+    case 0:
+      digitalWrite (3,HIGH);
+      digitalWrite (4,HIGH);
+      digitalWrite (5,HIGH);
+      digitalWrite (7,HIGH);
+      digitalWrite (8,HIGH);
+      digitalWrite (9,HIGH);  
+      break;
+  }
+     
+}
+
+int i=0;
+
+void loop() {
+  
+  number(i);
+
+  delay (1000);
+
+  i++;
+
+  if(i=10){
+    i=0;
+  }
+}
--- gr5b/Denys_Klekots/Lab_3/light/light.ino
+++ gr5b/Denys_Klekots/Lab_3/light/light.ino
@@ -1,6 +1,5 @@
 void setup() {
-
-  pinMode(2,OUTPUT);
+  
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
